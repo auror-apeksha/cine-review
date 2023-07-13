fdbb896d40c126e796258de389820600dd0b7159
auror-apeksha <apeksharajput2@gmail.com>
2023-07-13 22:06:28 +0530

    Carousel and Card UI Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/home";
+import MovieList from "./components/MovieList";
+import MovieDetails from "./pages/movieDetails";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
-          <Route index element={<h1>sad </h1>}></Route>
-          <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
-          <Route
-            path="movies/:type"
-            element={<h1>Movies list page</h1>}
-          ></Route>
-          <Route path="/*" element={<h1>Erro page</h1>}></Route>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<MovieDetails />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1>Error page</h1>}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../assets/css/header.css";
+import logo from "../assets/cineReviewLogo.png";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerLeft">
+        <Link to="/">
+          <img className="header__icon" src={logo} alt="Cine Review Logo" />
+        </Link>
+        <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+          <span>Popular</span>
+        </Link>
+        <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+          <span>Top Rated</span>
+        </Link>
+        <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+          <span>Upcoming</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
